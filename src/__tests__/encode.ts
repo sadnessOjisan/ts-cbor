cd0a6c916ea954be77527431d097d71a96a6f48f
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-10 14:02:39 +0900

    chore buffer

--- src/__tests__/encode.ts
+++ src/__tests__/encode.ts
@@ -20,6 +20,7 @@ describe("encode", () => {
       expect(actual).toEqual(expected);
     });
     it("should be that aditional type value(25) next 2byte is uint8_t value, when input is 65535", () => {
+      // 19 FFFF # unsigned(65535)
       const expected = new Buffer([25, 255, 255]);
       const actual = encodeNumber(65535);
       expect(actual).toEqual(expected);
@@ -34,10 +35,34 @@ describe("encode", () => {
       const actual = encodeNumber(9007199254740990);
       expect(actual).toEqual(expected);
     });
+    // --------negative--------
+    it("should be 20 # negative(0) when input is -1", () => {
+      const expected = new Buffer([32]);
+      const actual = encodeNumber(-1);
+
+      expect(actual).toEqual(expected);
+    });
+    it("should be 38 FE # negative(254) when input is -255", () => {
+      const expected = new Buffer([56, 254]);
+      const actual = encodeNumber(-255);
+      expect(actual).toEqual(expected);
+    });
+    it("should be 39 FFFE # negative(65534) when input is -65535", () => {
+      const expected = new Buffer([57, 255, 254]);
+      const actual = encodeNumber(-65535);
+      expect(actual).toEqual(expected);
+    });
+    it("should be 3A FFFFFFFE # negative(4294967295) when input is -4294967295", () => {
+      const expected = new Buffer([58, 255, 255, 255, 254]);
+      const actual = encodeNumber(-4294967295);
+      console.log(expected);
+      console.log(actual);
+      expect(actual).toEqual(expected);
+    });
   });
   describe("encodeToUint16", () => {
     it("should be <Buffer ff ff> when input is 65535", () => {
-      const expected = Buffer.allocUnsafe(2); // FIXME: 下とくっつけるとnumberになるのなんで
+      const expected = Buffer.allocUnsafe(2);
       expected.writeUInt16BE(65535, 0);
       const actual = encodeToUint16(65535);
       expect(actual).toEqual(expected);
--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 type MEJOR_TYPE_IDENTIFIER_TYPE = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
-type MEJOR_TYPE_NAME_TYPE = "unsignedInteger";
+type MEJOR_TYPE_NAME_TYPE = "unsignedInteger" | "negativeInteger";
 
 type MEJOR_TYPE_TYPE = {
   [key in MEJOR_TYPE_NAME_TYPE]: {
@@ -12,6 +12,10 @@ type MEJOR_TYPE_TYPE = {
 export const mejorType: MEJOR_TYPE_TYPE = {
   unsignedInteger: {
     type: 0,
+    description: "整数",
+  },
+  negativeInteger: {
+    type: 1,
     description: "整数",
   },
 };
