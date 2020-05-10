41f6c09b87fadb88887c4460673f6277c82483a1
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-10 15:14:06 +0900

    use class

--- src/encode.ts
+++ src/encode.ts
@@ -46,38 +46,22 @@ export const encodeNumber = (input: number) => {
     // 符号なし
     if (input < 24) {
       // FIXME: 桁数大きいし16進数で比較した方が見やすいかも
-      const encoded = encodeToUint8(input);
-      return encoded;
+      return PositiveEncoder.ai023encode(input);
     } else if (input < 256) {
-      const b = Buffer.allocUnsafe(2);
-      b.writeUInt8(m | 24, 0);
-      b.writeUInt8(input, 1);
-      return b;
+      return PositiveEncoder.ai24encode(input);
     } else if (input < 65536) {
-      const b = Buffer.allocUnsafe(3);
-      b.writeUInt8(m | 25, 0);
-      b.writeUInt16BE(input, 1);
-      return b;
+      return PositiveEncoder.ai25encode(input);
     } else if (input < 4294967296) {
-      const b = Buffer.allocUnsafe(5);
-      b.writeUInt8(m | 26, 0);
-      b.writeUInt32BE(input, 1);
-      return b;
+      return PositiveEncoder.ai26encode(input);
     } else if (input < Number.MAX_SAFE_INTEGER) {
-      const SHIFT32 = 0x100000000;
-      const b = Buffer.allocUnsafe(9);
-      b.writeUInt8(m | 27, 0);
-      b.writeUInt32BE(Math.floor(input / SHIFT32), 1);
-      b.writeUInt32BE(input % SHIFT32, 5);
-      return b;
+      return PositiveEncoder.ai27encode(input);
     }
     throw new Error("unreached");
   } else {
     // 符号あり
     const newInput = -1 * input - 1;
     if (input > -24) {
-      const encoded = encodeToUint8(m | newInput);
-      return encoded;
+      return PositiveEncoder.ai023encode(m | newInput);
     } else if (input > -256) {
       const b = Buffer.allocUnsafe(2);
       b.writeUInt8(m | 24, 0);
@@ -105,18 +89,75 @@ export const encodeNumber = (input: number) => {
   }
 };
 
-const encodeToUint8 = (num: number) => {
-  console.log("<encodeToUint8> num:", num);
-  const b = Buffer.allocUnsafe(1);
-  b.writeUInt8(num, 0); // 8ビット符合無し整数を0オフセットで書き込む
-  return b;
-};
-
 export const encodeToUint16 = (num: number) => {
   const b = Buffer.allocUnsafe(2);
   b.writeUInt16BE(num, 0);
   return b;
 };
+
+/**
+ * 自然数のエンコード関数をまとめたクラス
+ */
+class PositiveEncoder {
+  /**
+   * additional informationが0-23の時の変換関数
+   * そのままuint8_tで書き込む
+   * @param num
+   */
+  static ai023encode(num: number) {
+    const b = Buffer.allocUnsafe(1);
+    b.writeUInt8(num, 0); // 8ビット符合無し整数を0オフセットで書き込む
+    return b;
+  }
+
+  /**
+   * additional informationが24の時の変換関数
+   * uint8_tで書き込む
+   * @param num
+   */
+  static ai24encode(num: number) {
+    const b = Buffer.allocUnsafe(2);
+    b.writeUInt8(24, 0);
+    b.writeUInt8(num, 1);
+    return b;
+  }
+
+  /**
+   * additional informationが25の時の変換関数
+   * uint16_tで書き込む
+   * @param num
+   */
+  static ai25encode(num: number) {
+    const b = Buffer.allocUnsafe(3);
+    b.writeUInt8(25, 0);
+    b.writeUInt16BE(num, 1);
+    return b;
+  }
+
+  /**
+   * additional informationが26の時の変換関数
+   * uint32_tで書き込む
+   * @param num
+   */
+  static ai26encode(num: number) {
+    const b = Buffer.allocUnsafe(5);
+    b.writeUInt8(26, 0);
+    b.writeUInt32BE(num, 1);
+    return b;
+  }
+
+  /**
+   * additional informationが27の時の変換関数
+   * uint32_tで書き込む
+   * @param num
+   */
+  static ai27encode(num: number) {
+    const b = Buffer.allocUnsafe(9);
+    b.writeUInt8(27, 0);
+    b.writeBigInt64BE(BigInt(num), 1);
+    return b;
+  }
+}
 
 // const encodeBigint = (objs: any) => {
 //   const encoded = "hogehoge";
