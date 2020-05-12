29f4cefe4a2fb28eb5f397a7f93fc014afddd60f
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-12 16:19:33 +0900

    chore test

--- src/encoder/StringEncoder.ts
+++ src/encoder/StringEncoder.ts
@@ -16,21 +16,39 @@ export class StringEncoder {
   static shiftedMajorType = majorType.text.type << 5;
 
   /**
-   * 例) a => 61 61(unicode表) => 011_00011 xxxxxxxx = 99 xx => 0x63 0xXX
-   * 例) aaa => 63 61(unicode表) => 011_00100 xxxxxxxx = 101 xx => 0x63 0xXX
+   * 受け取った文字列をCBORに変換する。
+   * data item headerはmajor typeと長さの情報を入れる。
+   * 長さが23までならそのままdata item headerにそのまま長さを入れる。
+   * 長さが24より大きくなるなら data item header の次のバイトで長さを指定する。
+   * 2^8以上の長さがあり足りない場合は、必要に応じて次のbyteを使っていく。
+   * FYI: https://en.wikipedia.org/wiki/CBOR
+   *
+   * 例) a => 61 61(unicode表) => 011_00011 xxxxxxxx = 99 xx
+   * 例) aaa => 63 61(unicode表) 61 61 => 011_00100 xxxxxxxx = 101 xx
    * 例) aaaaaaaaaaaaaaaaaaaaaaa => 77 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61(unicode表) => 010_010101 xxxxxxxx = 101 xx => 0x63 0xXX
    * 例) aaaaaaaaaaaaaaaaaaaaaaaa => 0x78 0x18(長さ) 0x61 0x61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61(unicode表) => 011_11000 000_11000 xxxxxxxx = 101 xx => 0x63 0xXX
-   * @param str
+   * @param str 変換したい文字列
+   * @returns Buffer バイト列
    */
   static stringEncode(str: string) {
-    const length = str.length;
-    if (length < 24) {
-      const b = Buffer.allocUnsafe(length + 1);
-      b.writeUInt8(length | this.shiftedMajorType, 0);
+    const byteLength = encodeURIComponent(str).replace(/%../g, "x").length;
+    if (byteLength < 24) {
+      const b = Buffer.allocUnsafe(byteLength + 1);
+      b.writeUInt8(byteLength | this.shiftedMajorType, 0);
       b.write(str, 1);
       return b;
-    } else if (length === 24) {
-      // no op
+    } else if (byteLength < 256) {
+      const b = Buffer.allocUnsafe(byteLength + 2);
+      b.writeUInt8(this.shiftedMajorType | 24, 0);
+      b.writeUInt8(byteLength, 1);
+      b.write(str, 2);
+      return b;
+    } else if (byteLength < 65536) {
+      const b = Buffer.allocUnsafe(byteLength + 3);
+      b.writeUInt8(this.shiftedMajorType | 25, 0);
+      b.writeUInt16BE(byteLength, 1);
+      b.write(str, 3);
+      return b;
     }
   }
 }
--- src/encoder/__tests__/StringEncoder.ts
+++ src/encoder/__tests__/StringEncoder.ts
@@ -1,11 +1,27 @@
 import { StringEncoder } from "../StringEncoder";
 
-describe("NegativeEncoder", () => {
-  describe("ai023encode", () => {
-    it("should be <Buffer 61 61>, when input is -1", () => {
+describe("StringEncoder", () => {
+  describe("stringEncode", () => {
+    it("should be <Buffer 61 61>, when input is a", () => {
       const expected = new Buffer([97, 97]);
       const actual = StringEncoder.stringEncode("a");
-      console.log(actual);
+      expect(actual).toEqual(expected);
+    });
+    it("should be <Buffer 77 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61>, when input is aaaaaaaaaaaaaaaaaaaaaaa", () => {
+      const expected = new Buffer([119].concat(new Array(23).fill(97)));
+      const actual = StringEncoder.stringEncode("aaaaaaaaaaaaaaaaaaaaaaa");
+      expect(actual).toEqual(expected);
+    });
+    it("should be <Buffer 78 18 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61>, when input is aaaaaaaaaaaaaaaaaaaaaaaa", () => {
+      const expected = new Buffer([120, 24].concat(new Array(24).fill(97)));
+      const actual = StringEncoder.stringEncode("aaaaaaaaaaaaaaaaaaaaaaaa");
+      expect(actual).toEqual(expected);
+    });
+    it("should be <Buffer 79 01 00 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 ... 209 more bytes>, when input is aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", () => {
+      const expected = new Buffer([121, 1, 0].concat(new Array(256).fill(97)));
+      const actual = StringEncoder.stringEncode(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      );
       expect(actual).toEqual(expected);
     });
   });
