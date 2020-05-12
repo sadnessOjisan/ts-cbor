ee895fb2a6f4ad41f145244e81a8ec9f77578cf5
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-13 05:05:30 +0900

    fix byte

--- src/encoder/StringEncoder.ts
+++ src/encoder/StringEncoder.ts
@@ -50,13 +50,13 @@ export class StringEncoder {
       b.write(str, 3);
       return b;
     } else if (byteLength < 4294967296) {
-      const b = Buffer.allocUnsafe(byteLength + 4);
+      const b = Buffer.allocUnsafe(byteLength + 5);
       b.writeUInt8(this.shiftedMajorType | 26, 0);
       b.writeUInt32BE(byteLength, 1);
       b.write(str, 5);
       return b;
     } else if (byteLength < Number.MAX_SAFE_INTEGER) {
-      const b = Buffer.allocUnsafe(byteLength + 5);
+      const b = Buffer.allocUnsafe(byteLength + 9);
       b.writeUInt8(this.shiftedMajorType | 27, 0);
       b.writeBigInt64BE(BigInt(byteLength), 1);
       b.write(str, 9);
--- src/encoder/__tests__/StringEncoder.ts
+++ src/encoder/__tests__/StringEncoder.ts
@@ -14,22 +14,22 @@ describe("StringEncoder", () => {
     });
     it("should be <Buffer 78 18 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61>, when input is aaaaaaaaaaaaaaaaaaaaaaaa", () => {
       const expected = new Buffer([120, 24].concat(new Array(24).fill(97)));
-      const actual = StringEncoder.stringEncode("aaaaaaaaaaaaaaaaaaaaaaaa");
+      const actual = StringEncoder.stringEncode("a".repeat(24));
       expect(actual).toEqual(expected);
     });
     it("should be <Buffer 79 01 00 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 ... 209 more bytes>, when input is aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", () => {
       const expected = new Buffer([121, 1, 0].concat(new Array(256).fill(97)));
-      const actual = StringEncoder.stringEncode(
-        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      const actual = StringEncoder.stringEncode("a".repeat(256));
+      expect(actual).toEqual(expected);
+    });
+    it("should be<Buffer 7a 00 01 00 01 61 61 61 61 61... >, when input is a x 65537", () => {
+      const expected = new Buffer(
+        [122, 0, 1, 0, 1].concat(new Array(65537).fill(97))
       );
+      const actual = StringEncoder.stringEncode("a".repeat(65537));
       expect(actual).toEqual(expected);
     });
-    it("should be <Buffer 79 01 00 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 61 ... 209 more bytes>, when input is aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", () => {
-      const expected = new Buffer([121, 1, 0].concat(new Array(256).fill(97)));
-      const actual = StringEncoder.stringEncode("a");
-      expect(actual).toEqual(expected);
-    });
-    it("should be <Buffer 63 e3 81 82>, when input is a", () => {
+    it("(unicode) should be <Buffer 63 e3 81 82>, when input is あ", () => {
       const expected = new Buffer([99, 227, 129, 130]);
       const actual = StringEncoder.stringEncode("あ");
       expect(actual).toEqual(expected);
