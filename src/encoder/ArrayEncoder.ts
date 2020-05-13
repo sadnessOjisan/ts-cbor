fe710cede7199ddcb6311eade0906c71f4508110
sadnessOjisan <sadness.ojisan@gmail.com>
2020-05-13 21:48:29 +0900

    impl array (#7)
    * chore simple array
    
    * array test
    
    * wip type

---
+++ src/encoder/ArrayEncoder.ts
@@ -0,0 +1,71 @@
+/**
+ * @file primitiveのエンコード. エンコードルールは https://tools.ietf.org/html/rfc7049#section-2.1 のMajor Type 7を参照.
+ */
+
+import { majorType } from "../const";
+import { Encoder } from "./Encoder";
+
+/**
+ * 配列のエンコード関数をまとめたクラス.
+ * Major Typeは4.
+ */
+export class ArrayEncoder {
+  /**
+   * 1byteで表現されるdata item headerの先頭3bit.
+   * 8bitあるうちの先頭3bitにcategory typeを持ってきたいのでmajor typeを5bitシフトさせる.
+   */
+  static shiftedMajorType = majorType.array.type << 5;
+
+  /**
+   * primitiveのencodeを行う.
+   * @param num primitiveを表す数値
+   */
+  static arrayEncode(arr: any[]) {
+    const length = arr.length;
+    if (length < 24) {
+      const dataItemHeader = this.shiftedMajorType | length;
+      let b = Buffer.allocUnsafe(1);
+      b.writeUInt8(dataItemHeader, 0); // FIXME: magic number
+      for (const a of arr) {
+        b = Buffer.concat([b, Encoder.encodeAny(a)]);
+      }
+      return b;
+    } else if (length < 256) {
+      const dataItemHeader = this.shiftedMajorType | 24;
+      let b = Buffer.allocUnsafe(2); // 先頭1bybeにadditional info, 次の1byteに長さを入れる. FIXME: magic number
+      b.writeUInt8(dataItemHeader, 0); // FIXME: magic number
+      b.writeUInt8(length, 1);
+      for (const a of arr) {
+        b = Buffer.concat([b, Encoder.encodeAny(a)]);
+      }
+      return b;
+    } else if (length < 65536) {
+      const dataItemHeader = this.shiftedMajorType | 25; // FIXME: magic number
+      let b = Buffer.allocUnsafe(3); // 先頭1bybeにadditional info, 次の2byteに長さを入れる. FIXME: magic number
+      b.writeUInt8(dataItemHeader, 0);
+      b.writeUInt16BE(length, 1); // FIXME: magic number
+      for (const a of arr) {
+        b = Buffer.concat([b, Encoder.encodeAny(a)]);
+      }
+      return b;
+    } else if (length < 4294967296) {
+      const dataItemHeader = this.shiftedMajorType | 26; // FIXME: magic number
+      let b = Buffer.allocUnsafe(5); // 先頭1bybeにadditional info, 次の2byteに長さを入れる. FIXME: magic number
+      b.writeUInt8(dataItemHeader, 0);
+      b.writeUInt32BE(length, 1); // FIXME: magic number
+      for (const a of arr) {
+        b = Buffer.concat([b, Encoder.encodeAny(a)]);
+      }
+      return b;
+    } else if (length < Number.MAX_SAFE_INTEGER) {
+      const dataItemHeader = this.shiftedMajorType | 27; // FIXME: magic number
+      let b = Buffer.allocUnsafe(9); // 先頭1bybeにadditional info, 次の2byteに長さを入れる. FIXME: magic number
+      b.writeUInt8(dataItemHeader, 0);
+      b.writeBigInt64BE(BigInt(length), 1); // FIXME: magic number
+      for (const a of arr) {
+        b = Buffer.concat([b, Encoder.encodeAny(a)]);
+      }
+      return b;
+    }
+  }
+}
---
+++ src/encoder/__tests__/ArrayEncoder.ts
@@ -0,0 +1,65 @@
+import { ArrayEncoder } from "../ArrayEncoder";
+
+describe("ArrayEncoder", () => {
+  describe("stringEncode", () => {
+    it("should be <Buffer 81 61 61>, when input is ['a']", () => {
+      const expected = new Buffer([129, 97, 97]);
+      const actual = ArrayEncoder.arrayEncode(["a"]);
+      expect(actual).toEqual(expected);
+    });
+    it("should be <Buffer 98 ff 61 61 61...>, when input is new Array(255).fill(a)", () => {
+      //  61 61 でa. つまりaを255個並べるためには97(=0x61, unicodeでa)が510個並ぶ必要がある
+      const expected = new Buffer([152, 255].concat(new Array(510).fill(97)));
+      const actual = ArrayEncoder.arrayEncode(new Array(255).fill("a"));
+      expect(actual).toEqual(expected);
+    });
+  });
+  it("should be <Buffer 99 01 00 61 61...>, when input is new Array(256).fill(a)", () => {
+    //  61 61 でa. つまりaを256個並べるためには97(=0x61, unicodeでa)が512個並ぶ必要がある
+    const expected = new Buffer([153, 1, 0].concat(new Array(512).fill(97)));
+    const actual = ArrayEncoder.arrayEncode(new Array(256).fill("a"));
+    expect(actual).toEqual(expected);
+  });
+  it("should be <Buffer 99 ff ff 61 61...>, when input is new Array(65535).fill(a)", () => {
+    //  61 61 でa. つまりaを65535個並べるためには97(=0x61, unicodeでa)が65535*2個並ぶ必要がある
+    const expected = new Buffer(
+      [153, 255, 255].concat(new Array(65535 * 2).fill(97))
+    );
+    const actual = ArrayEncoder.arrayEncode(new Array(65535).fill("a"));
+    expect(actual).toEqual(expected);
+  });
+  it("should be <Buffer 9a 00 01 00 00 61 61 61...>, when input is new Array(65536).fill(a)", () => {
+    //  61 61 でa. つまりaを65536個並べるためには97(=0x61, unicodeでa)が65536*2個並ぶ必要がある
+    const expected = new Buffer(
+      [154, 0, 1, 0, 0].concat(new Array(65536 * 2).fill(97))
+    );
+    const actual = ArrayEncoder.arrayEncode(new Array(65536).fill("a"));
+    expect(actual).toEqual(expected);
+  });
+  // memory足りないから実行できない
+  test.todo(
+    "should be <Buffer 99 01 00 61 61...>, when input is new Array(4294967295).fill(a)"
+  );
+  // memory足りないから実行できない
+  test.todo(
+    "should be <Buffer 99 01 00 61 61...>, when input is new Array(4294967296).fill(a)"
+  );
+  describe("multi byte文字", () => {
+    it("should be <Buffer 81 63 e3 81 82>, when input is [あ]", () => {
+      const expected = new Buffer([129, 99, 227, 129, 130]);
+      const actual = ArrayEncoder.arrayEncode(["あ"]);
+      expect(actual).toEqual(expected);
+    });
+    it("should be <Buffer 98 ff 63 e3 81 82 63 e3 81 82 63 e3 81 82>, when input is [あ]", () => {
+      const expected = new Buffer(
+        [152, 255].concat(
+          Array.from(new TextEncoder().encode("あ".repeat(255)))
+        )
+      );
+      const actual = ArrayEncoder.arrayEncode(new Array(255).fill("あ"));
+      console.log(expected);
+      console.log(actual);
+      //   expect(actual).toEqual(expected);
+    });
+  });
+});
