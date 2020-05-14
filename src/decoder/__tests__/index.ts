a321a1dbc72cd18f84faebcfd1d451891c6056ac
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-15 02:12:33 +0900

    chore num

--- src/decoder/__tests__/index.ts
+++ src/decoder/__tests__/index.ts
@@ -1,11 +1,64 @@
 import { Decoder } from "../index";
 
 describe("Decoder", () => {
-  describe("stringEncode", () => {
-    it("should be 21, when input is 15", () => {
-      const expected = 21;
-      const actual = Decoder.decode("15");
-      expect(actual).toEqual(expected);
+  describe("plus number decoder", () => {
+    describe("additional info 0-23", () => {
+      it("should be 0, when input is 0", () => {
+        const expected = 0;
+        const actual = Decoder.decode("0");
+        expect(actual).toEqual(expected);
+      });
+      it("should be 23, when input is 17", () => {
+        const expected = 23;
+        const actual = Decoder.decode("17");
+        expect(actual).toEqual(expected);
+      });
+    });
+    describe("additional info 24", () => {
+      it("should be 24, when input is 1818", () => {
+        const expected = 24;
+        const actual = Decoder.decode("1818");
+        expect(actual).toEqual(expected);
+      });
+      it("should be 255, when input is 18FF", () => {
+        const expected = 255;
+        const actual = Decoder.decode("18FF");
+        expect(actual).toEqual(expected);
+      });
+    });
+    describe("additional info 25", () => {
+      it("should be 256, when input is 190100", () => {
+        const expected = 256;
+        const actual = Decoder.decode("190100");
+        expect(actual).toEqual(expected);
+      });
+      it("should be 65535, when input is 19FFFF", () => {
+        const expected = 65535;
+        const actual = Decoder.decode("19FFFF");
+        expect(actual).toEqual(expected);
+      });
+    });
+    describe("additional info 26", () => {
+      it("should be 65536, when input is 1A00010000", () => {
+        const expected = 65536;
+        const actual = Decoder.decode("1A00010000");
+        expect(actual).toEqual(expected);
+      });
+      it("should be 4294967295, when input is 1AFFFFFFFF", () => {
+        const expected = 4294967295;
+        const actual = Decoder.decode("1AFFFFFFFF");
+        expect(actual).toEqual(expected);
+      });
+    });
+    describe("additional info 27", () => {
+      it("should be 4294967296, when input is 1B0000000100000000", () => {
+        const expected = 4294967296;
+        const actual = Decoder.decode("1B0000000100000000");
+        expect(actual).toEqual(expected);
+      });
+      test.todo(
+        "Number.MAX_SAFE_INTEGER を対象にテストしたいが溢れてしまうのでテストできない"
+      );
     });
   });
 });
--- src/decoder/index.ts
+++ src/decoder/index.ts
@@ -6,23 +6,36 @@ export class Decoder extends Stream.Transform {
     const bitLength = sbinput.length;
     const needZero = 8 - (bitLength % 8);
     const paddedBit = "0".repeat(needZero) + sbinput;
-    console.log("[Decoder]<decode> paddedBit ", paddedBit);
     const addedInfo = paddedBit.slice(0, 8);
     const majorTypeString = addedInfo.slice(0, 3);
     const majorType = toNum(toNum(majorTypeString).toString(10));
-    console.log("[Decoder]<decode> addedInfo ", addedInfo);
-    console.log("[Decoder]<decode> majorType ", majorType);
     if (majorType === 0) {
       // 整数ケース
       const infoNum = toNum(parseInt(addedInfo, 2).toString(10));
-      console.log("[Decoder]<decode> infoNum ", infoNum);
       if (infoNum < 24) {
         // addedInfoがそのまま数字になる
         return infoNum;
-      } else if (infoNum < 256) {
+      } else if (infoNum === 24) {
         // addedInfoの次8bitが数値
         const res = paddedBit.slice(8, 16);
         const ret = toNum(parseInt(res, 2).toString(10));
+        return ret;
+      } else if (infoNum === 25) {
+        // addedInfoの次18bitが数値
+        const res = paddedBit.slice(8, 24);
+        const ret = toNum(parseInt(res, 2).toString(10));
+        return ret;
+      } else if (infoNum === 26) {
+        // addedInfoの次32bitが数値
+        const res = paddedBit.slice(8, 40);
+        const ret = toNum(parseInt(res, 2).toString(10));
+        console.log(paddedBit);
+        return ret;
+      } else if (infoNum === 27) {
+        // addedInfoの次64bitが数値
+        const res = paddedBit.slice(8, 72);
+        const ret = toNum(parseInt(res, 2).toString(10));
+        console.log(paddedBit);
         return ret;
       }
     }
