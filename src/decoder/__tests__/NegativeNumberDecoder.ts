323a9af40a6baea18c70d9b0a16bb306710c6244
sadness_ojisan <sadness.ojisan@gmail.com>
2020-05-17 14:10:44 +0900

    negative number

--- src/decoder/__tests__/NegativeNumberDecoder.ts
+++ src/decoder/__tests__/NegativeNumberDecoder.ts
@@ -1,14 +1,33 @@
+import { toCBOR } from "../../helper";
+import { NegativeNumberDecoder } from "../NegativeNumberDecoder";
+
 describe("NegativeNumberDecoder", () => {
   describe("decode", () => {
     describe("-0~-24へのdecode", () => {
-      test.todo("入力が0x20の時、出力は-1");
-      test.todo("入力が0x37の時、出力は-24");
+      test("入力が0x20の時、出力は-1", () => {
+        const input = toCBOR("20");
+        const actual = NegativeNumberDecoder.decode(input);
+        expect(actual).toBe(-1);
+      });
+      test("入力が0x37の時、出力は-24", () => {
+        const input = toCBOR("37");
+        const actual = NegativeNumberDecoder.decode(input);
+        expect(actual).toBe(-24);
+      });
     });
     describe("-25以以下へのdecode", () => {
-      test.todo("入力が0x3818の時、出力は-25");
+      test("入力が0x3818の時、出力は-25", () => {
+        const input = toCBOR("3818");
+        const actual = NegativeNumberDecoder.decode(input);
+        expect(actual).toBe(-25);
+      });
     });
     describe("巨大な負の数へのdecode", () => {
-      test.todo("入力が0x3B0000000100000000の時、出力は-4294967297");
+      test("入力が0x3818の時、出力は入力が0x3B0000000100000000の時、出力は", () => {
+        const input = toCBOR("3B0000000100000000");
+        const actual = NegativeNumberDecoder.decode(input);
+        expect(actual).toBe(-4294967297);
+      });
     });
   });
 });
